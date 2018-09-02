c91a407cf9266e69cb1fd4269b3e73a595507924
Ashley Barkworth <ashley.barkworth@gmail.com>
2018-09-02 14:51:18 -0700

    removed upvoted submissions option, added greyscale option

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,11 +1,16 @@
 import praw
+from prawcore import NotFound
 import config
 import time
 import requests
 import argparse
 import os
+from PIL import Image
+import urllib2 as urllib
+from cStringIO import StringIO
 
 image_directory = os.path.expanduser('~') + '/Documents/images/'
+image_count = 1
 
 def bot_login():
 	loggedIn = False
@@ -15,139 +20,137 @@ def bot_login():
 				password = config.password,
 				client_id = config.client_id,
 				client_secret = config.client_secret,
-				user_agent = "ImageSaverBot (by /u/yelhsa08)")
+				user_agent = 'ImageSaverBot (by /u/yelhsa08)')
 			loggedIn = True
 		except Exception as e:
 			print e
 
 	return reddit
 
-def save_images(images):
-	count = 1
-	for image in images:
-		file_name = '{}-{}{}'.format('images', count, '.jpg')
-		with open(image_directory + file_name, "wb") as handler:
-			handler.write(image)
-		count += 1
+def get_link_type(url):
+	try:
+		r = requests.get(url)
+		return r.headers['content-type']
+	except requests.exceptions.RequestException as e:
+		print 'Oops! Something fuckey''s going on...:', e
+		pass
+	except requests.exceptions.HTTPError as e:
+		print 'Http Error: ', e
+		pass
+	except requests.exceptions.Timeout as e:
+		print 'Timeout Error: ', e
+		pass
+	except requests.exceptions.ConnectionError as e:
+		print 'Error Connecting: ', e 
+		pass	 
 
+def is_image(url):
+	content_type = get_link_type(url)
+	image_types = ['image/jpeg', 'image/jpg', 'image/png']
+	return content_type in image_types
 
-def detect_images(urls):
-	print 'Let''s see if these links contain images...'
-	imgTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
+def convert_to_greyscale(image):
+	return image.convert('L')
+
+def create_directory():
+	if not os.path.exists(os.path.abspath(image_directory)):
+		os.makedirs(os.path.abspath(image_directory))
+
+def save_images(image_urls, greyscale):
+	global image_count
+	for url in image_urls:
+		# Save image as the type indicated in the submission URL
+		extension = ''
+		if get_link_type(url) == 'image/jpeg' or get_link_type(url) == 'image/jpg':
+			extension = 'jpg'
+		elif get_link_type(url) == 'image/png':
+			extension = 'png'
+
+		# create PIL Image to save and use for transformations
+		image_file = urllib.urlopen(url)
+		image_content = StringIO(image_file.read())
+		image = Image.open(image_content)
+		if greyscale:
+			image = convert_to_greyscale(image)
+		file_name = '{}-{}.{}'.format('image', image_count, extension)
+		image.save(image_directory + file_name)
+
+		# enumerate image_count
+		image_count += 1
+
+def get_subreddit_images(reddit, subreddits, num):
+	subreddit_list = ''
+	for subreddit in subreddits:
+		subreddit_list += subreddit + '+'
+	subreddit_list = subreddit_list[:-1]
+
 	images = []
-	count = 1
-	for url in urls:
-		print 'Looking at URL ' + str(count)
-		try:
-			r = requests.get(url)
-			link_type = r.headers['content-type']
-			if link_type in imgTypes:
-				print 'Found an image'
-				images.append(r.content)
-		except requests.exceptions.RequestException as e:
-			print 'Oops! Something fuckey''s going on...:', e
-			pass
-		except requests.exceptions.HTTPError as e:
-			print 'Http Error: ', e
-			pass
-		except requests.exceptions.Timeout as e:
-			print 'Timeout Error: ', e
-			pass
-		except requests.exceptions.ConnectionError as e:
-			print 'Error Connecting: ', e 
-			pass
-		count = count + 1
-
-	print 'Found the images'
+	submissions = reddit.subreddit(subreddit_list).hot()
+	
+	while len(images) < num:
+		submission = submissions.next()
+		if is_image(submission.url):
+			images.append(submission.url)
+		time.sleep(1)
 
 	return images
 
-
-def get_top_submissions(subreddit):
-	top_submissions = []
-	submission_ids = subreddit.hot(limit=25)
-	for id in submission_ids:
-		top_submissions.append(reddit.submission(id).url)	
-	return top_submissions
-
-
-
-def get_subreddit_images(reddit, subreddits):
-	images = []
+def subreddit_exists(reddit, subreddits):
+	non_existent_subs = []
 	for subreddit in subreddits:
-		submissions = subreddit.hot(limit=25)
-		images.append(detect_images(submissions))
-
-
-
-def save_images_from_subreddits(reddit, subreddits):
-	print 'Let''s go!'
-
-	print 'Getting subreddits...'
-	subreddit_list = ''
-	
-	for subreddit in subreddits:
-		subreddit_list += subreddit + '+'
-
-	subreddit_list = subreddit_list[:-1]
-
-	submission_urls = []
-	submissions = reddit.subreddit(subreddit_list).hot(limit=25)
-
-	for submission in submissions:
-		submission_urls.append(submission.url)
-
-	print 'Detecting images'
-	images = detect_images(submission_urls)
-
-	print 'Saving images'
-
-	save_images(images)
-
-	print 'Done'
-
-def save_upvoted_image_submissions(reddit):
-	print 'Let''s go!'
-
-	print 'Finding upvoted submissions...'
-	redditor = reddit.redditor(config.username)
-	upvoted_submissions_ids = redditor.upvoted(limit=10)
-
-	submissions = []
-	for id in upvoted_submissions_ids:
-		print 'Submission url: ' + reddit.submission(id).url
-		submissions.append(reddit.submission(id).url)
-
-	print 'Done'
-	images = detect_images(submissions)
-
-	print 'Saving images'
-
-	save_images(images)
-
-	print 'Done'
+		try:
+			reddit.subreddits.search_by_name(subreddit, exact=True)
+		except NotFound:
+			non_existent_subs.append(subreddit)
+	return non_existent_subs
 
 def parse_args():
-	parser = argparse.ArgumentParser(description='Image Saver')
-	parser.add_argument('-s', '--subreddits', action='store_true', help='choose a list of subreddits to save images from')
-	parser.add_argument('-u', '--upvoted', action='store_true', help='save images from your latest upvoted submissions')
+	parser = argparse.ArgumentParser(description='Image Saver Bot')
+	parser.add_argument('-g', '--greyscale', action='store_true', help='indicates whether the images saved are converted to greyscale')
 	args = parser.parse_args()
 	return args
 
-def main():
+def run_bot():
 	print 'Image Saver Reddit bot by /u/yelhsa08'
 
+	# Login to reddit w/ user credentials
 	reddit = bot_login()
 
+	# Parse arguments for image transformations
 	args = parse_args()
+	greyscale = args.greyscale
 
-	if args.subreddits:
-		print 'Let''s get some images from subreddits'
-		subreddit_input = raw_input('Enter a comma-separated list of subreddits (e.g. wallpaper,EarthPorn,multiwall): ')
-		subreddits = subreddit_input.split(',')
-		save_images_from_subreddits(reddit, subreddits)
-	elif args.upvoted:
-		print "Let's look in your upvoted submissions"
-		save_upvoted_image_submissions(reddit)
+	# Parse subreddits from user input, filtering out empty items and finding any subreddits that do not exist
+	subreddit_input = raw_input('Enter a comma-separated list of subreddits: ')
+	subreddits = list(filter(None, subreddit_input.split(',')))
+	subs_do_not_exist = subreddit_exists(reddit,subreddits)
 
-main()
+	# Keep prompting the user to provide subreddits until all inputted subreddits exist; otherwise, notify which subreddits do not
+	while len(subs_do_not_exist) > 0:
+		print 'Woops! Couldn\'t find the following subreddits: {}'.format(subs_do_not_exist)
+		subreddit_input = raw_input('Enter a comma-separated list of subreddits: ')
+		subreddits = list(filter(None, subreddit_input.split(',')))
+		subs_do_not_exist = subreddit_exists(reddit,subreddits)
+
+	# Parse user input to get number of images to save, and keep trying until a valid integer is inputted
+	valid_num = False
+	while not valid_num:
+		try:
+			num_images = int(raw_input('Enter the number of images you want saved: '))
+			valid_num = True
+		except ValueError:
+			print 'Input cannot be parsed as an integer. Please try again'
+			continue
+
+	# Create directory to save images to
+	create_directory()
+
+	# Get urls of the top image submissions from the inputted list of subreddits
+	image_urls = get_subreddit_images(reddit, subreddits, num_images)
+
+	# Save images to directory
+	save_images(image_urls, greyscale)
+
+	print 'Done! Check the directory for your images'
+
+run_bot()
